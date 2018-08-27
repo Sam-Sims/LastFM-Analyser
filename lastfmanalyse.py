76c9ac44954a6d6ac9158a20529a01ba70a7dad9
Sam <samsimss98@gmail.com>
2018-08-27 18:59:39 +0100

    Make_label is now generic function to format any label from two columns in a dataframe.
    Top albums are graphed now as well.

--- lastfmanalyse.py
+++ lastfmanalyse.py
@@ -28,25 +28,27 @@ def analyse_top_artists(graph_settings):
     plt.show()
 
 
-def make_label(row, maxlength=30, suffix='...'):
-    artist = row['artist']
-    track = row['track']
-    if len(track) > maxlength:
-        track = '{}{}'.format(track[:maxlength-len(suffix)], suffix)
-    return '{}\n{}'.format(artist, track)
+def make_label(df, top, bot):
+    maxlength = 30
+    suffix = '...'
+    top_value = df[top]
+    bot_value = df[bot]
+    if len(bot_value) > maxlength:
+        bot_value = '{}{}'.format(bot_value[:maxlength-len(suffix)], suffix)
+    return '{}\n{}'.format(top_value, bot_value)
+
 
 def analyse_top_tracks(graph_settings):
     top_tracks = pd.read_csv('data/lastfm_top_tracks.csv', encoding='utf-8')
 
-    index = top_tracks.apply(make_label, axis='columns')
-    tracks_most = top_tracks.set_index(index).drop(labels=['artist', 'track'], axis='columns')
-    tracks_most = tracks_most['play_count'].head(20)
-    tracks_most.head()
-    print(tracks_most)
+    index = top_tracks.apply(make_label, args=('artist', 'track'), axis='columns')
+    top_tracks = top_tracks.set_index(index).drop(labels=['artist', 'track'], axis='columns')
+    top_tracks = top_tracks['play_count'].head(20)
+    top_tracks.head()
 
-    ax = tracks_most.sort_values().plot(kind='barh', figsize=[6, 10], width=0.8, alpha=0.6, color='#ce6c31', edgecolor=None, zorder=2)
+    ax = top_tracks.sort_values().plot(kind='barh', figsize=[6, 10], width=0.8, alpha=0.6, color='#ce6c31', edgecolor=None, zorder=2)
     ax.xaxis.grid(True)
-    ax.set_title('Top tracks', fontproperties=graph_settings.title_font)
+    ax.set_title('Top Tracks', fontproperties=graph_settings.title_font)
     ax.set_xlabel('', fontproperties=graph_settings.label_font)
     ax.set_ylabel('Track', fontproperties=graph_settings.label_font)
     ax.set_xlabel('Playcount', fontproperties=graph_settings.label_font)
@@ -54,10 +56,27 @@ def analyse_top_tracks(graph_settings):
     plt.show()
 
 
+def analyse_top_albums(graph_settings):
+    top_albums = pd.read_csv('data/lastfm_top_albums.csv', encoding='utf-8')
+    index = top_albums.apply(make_label, args=('artist', 'albums'), axis='columns')
+    top_albums = top_albums.set_index(index).drop(labels=['artist', 'albums'], axis='columns')
+    top_albums = top_albums['playcount'].head(20)
+    top_albums.head()
+    ax = top_albums.sort_values().plot(kind='barh', figsize=[6, 10], width=0.8, alpha=0.6, color='#ce6c31', edgecolor=None, zorder=2)
+
+    ax.xaxis.grid(True)
+    ax.set_title('Top Albums', fontproperties=graph_settings.title_font)
+    ax.set_xlabel('', fontproperties=graph_settings.label_font)
+    ax.set_ylabel('Album', fontproperties=graph_settings.label_font)
+    ax.set_xlabel('Playcount', fontproperties=graph_settings.label_font)
+    plt.savefig('images/lastfm-albums-played-most.png', bbox_inches='tight', dpi=100)
+    plt.show()
+
+
 def main():
     graph_settings = GraphSettings()
     #analyse_top_artists(graph_settings)
-    analyse_top_tracks(graph_settings)
+    analyse_top_albums(graph_settings)
 
 
 if __name__ == "__main__":
